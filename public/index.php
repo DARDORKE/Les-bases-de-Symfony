1a59cc8819b5a7350da0796d57b307a12dd8eefc
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-05 15:12:36 +0200

    Le système de routing 1/2

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+//Définissez une route post_list: affichant l'ensemble des articles d'un blog.
+// La méthode affichant les articles se nomme list et se trouve dans le contrôleur
+// PostController placé dans le dossier src/Controller.
+// L'URL de la route doit être https://www.example.com/liste-des-articles.
 
 use App\Kernel;
 
