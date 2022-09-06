d4471406ad16b1f0c5e591f452f71351171d36eb
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-06 15:53:27 +0200

    Utilisation basique d'un contrôleur 1/2

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
-
+// Créez votre premier contrôleur intitulé BasicController en vous aidant de la commande
+// Symfony make:controller, puis personnalisez le message affiché pour renvoyer une réponse
+// contenant du code HTML.
+//
+//Une fois votre contrôleur créé, installez la debug toolbar du
+// profiler Symfony au moyen de la commande composer req profiler,
+// et trouvez-y les informations concernant la Request et la Response.
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
