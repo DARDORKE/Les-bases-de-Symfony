806e1d38441dae397a164df86f12495c8a92a3ed
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-06 13:39:13 +0200

    La génération d'URL 2/2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,36 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class BlogController extends AbstractController
 {
     /**
      * @Route("/", name="blog_index")
      */
-    public function index()
+    public function index(): Response
     {
+        $url = $this->generateUrl('blog_posts_users_date', [
+            'user' => 'john',
+            'year' => 2015,
+        ]);
+
         return new Response(
-            '<html><body>Accueil</body></html>'
+            '<html><body>'.$url.'</body></html>'
         );
     }
 
     /**
      * @Route("/posts", name="blog_posts")
      */
-    public function posts()
+    public function posts(): Response
     {
         return new Response(
             '<html><body>Liste des articles</body></html>'
         );
     }
 
+
     /**
      * @Route(
      *    "/posts/{user}/{year}",
-     *    methods={"GET"},
+     *    name="blog_posts_users_date",
+     *    methods={"GET"}),
      *    requirements={"year"="^\d{4}$"}
-     * )
+     * }
      */
-    public function postsFromUserAndYear(string $user, int $year = 2022)
+    public function postsFromUserAndYear(string $user, int $year = 2022): Response
     {
         return new Response(
             '<html><body>'.$user.' '.$year.'</body></html>'
