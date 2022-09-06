804a2331735e2eba2f0bbd074c0f79601c496a39
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-06 11:14:46 +0200

    Le routing Symfony

--- src/Controller/AproposController.php
+++ src/Controller/AproposController.php
@@ -4,10 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AproposController extends AbstractController
 {
-
+    #[Route('/a-propos', name: 'a-propos')]
     public function index(): Response
     {
         return $this->render('apropos.html.twig', [
--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,10 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
 {
-
+    #[Route('/articles', name: 'articles')]
     public function index(): Response
     {
         return $this->render('articles.html.twig', [
