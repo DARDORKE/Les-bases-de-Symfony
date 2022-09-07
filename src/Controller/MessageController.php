7a49b2ed8596344047da9cf9427a6998296f0285
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-07 11:42:03 +0200

    AbstractController et les méthodes fournies 2/2

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -9,4 +9,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class MessageController extends AbstractController
 {
     public const MESSAGES = ['Bonjour', 'Bonsoir', 'Au revoir'];
+
+    #[Route("/messages", name: "message_list")]
+    public function list(): Response
+    {
+        $messages = [];
+
+        foreach (self::MESSAGES as $index => $message) {
+            $messages[] = [
+                'text' => $message,
+            ];
+        }
+
+        return $this->json($messages);
+    }
+
+    #[Route("/messages/{id}", name: "message_item")]
+    public function item(int $id): Response
+    {
+        if (!isset(self::MESSAGES[$id])) {
+            throw $this->createNotFoundException('Le message n\'existe pas');
+        }
+
+        return $this->json(self::MESSAGES[$id]);
+    }
 }
