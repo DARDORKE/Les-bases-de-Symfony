6ebd26b40dc547dcb8e290ca13d5e5496a4135d9
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-07 18:16:47 +0200

    Qu'est-ce qu'une entité ? 2/2

--- public/index.php
+++ public/index.php
@@ -1,4 +1,43 @@
 <?php
+//Imaginons que nous ayons besoin d'une entité permettant de gérer des livres pour une bibliothèque.
+//
+// Définissez une entité Book complète (classe, propriétés, getters et setters) correspondant à une table dont la
+// structure est la suivante :
+//
+//Table Book
+//Nom
+//
+//Type
+//
+//Null
+//
+//id
+//
+//int(11)
+//
+//Non
+//
+//isbn
+//
+//varchar(255)
+//
+//Non
+//
+//name
+//
+//varchar(255)
+//
+//Non
+//
+//date_published
+//
+//datetime
+//
+//Non
+//
+//La classe se situera dans le répertoire src/Entity/.
+//
+//On ne s'occupera pas des annotations dans cet exercice. L'objectif est de créer une simple classe PHP.
 
 use App\Kernel;
 
---
+++ src/Entity/Book.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_published = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDatePublished(): ?\DateTimeInterface
+    {
+        return $this->date_published;
+    }
+
+    public function setDatePublished(\DateTimeInterface $date_published): self
+    {
+        $this->date_published = $date_published;
+
+        return $this;
+    }
+}
