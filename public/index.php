aa9b1308bb5b4b29a3daac08621e21489d355a5a
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-07 12:16:52 +0200

    Manipuler l'objet Request 1/2

--- public/index.php
+++ public/index.php
@@ -1,36 +1,42 @@
 <?php
 
-// Vous disposez de l'extrait de code suivant, créez un service qui permettra d'externaliser
-// le contenu de la méthode MessageController::list():
-
+// En se basant sur l'exemple précédent enrichi de quelques messages, mettez en place un système
+// de pagination pour vos messages en ajoutant dans l'URL un paramètre page correspondant au numéro de la page.
+// Essayez par vous-même dans un premier temps. Si vous trouvez la tâche un peu trop ardue par cette
+// nouvelle notion de pagination, vous retrouverez dans la solution une aide vous expliquant pas à pas
+// comment la mettre en place.
 //<?php
 //
 //// src/Controller/MessageController.php
 //namespace App\Controller;
 //
+//use App\Helper\MessageHelper;
 //use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //use Symfony\Component\HttpFoundation\Response;
 //use Symfony\Component\Routing\Annotation\Route;
 //
 //class MessageController extends AbstractController
 //{
-//    public const MESSAGES = ['Bonjour', 'Bonsoir', 'Au revoir'];
+//    public const MESSAGES = ['Bonjour', 'Bonsoir', 'Au revoir', 'Salut', 'Coucou', 'Ciao', 'Adieu', 'Bienvenue', 'Bonne nuit', 'Bye'];
 //
 //    /**
 //     * @Route("/messages", name="message_list")
 //     */
-//    public function list(): Response
+//    public function list(MessageHelper $messageHelper): Response
 //    {
-//        $messagesWithURLs = [];
+//        return $this->json($messageHelper->addUrlsToMessages(self::MESSAGES));
+//    }
 //
-//        foreach (self::MESSAGES as $index => $message) {
-//            $messagesWithURLs[] = [
-//                'text' => $message,
-//                'url' => $this->generateUrl('message_item', ['id' => $index]),
-//            ];
+//    /**
+//     * @Route("/messages/{id}", name="message_item")
+//     */
+//    public function item(int $id): Response
+//    {
+//        if (!isset(self::MESSAGES[$id])) {
+//            throw $this->createNotFoundException('Le message n\'existe pas');
 //        }
 //
-//        return $this->json($messagesWithURLs);
+//        return $this->json(self::MESSAGES[$id]);
 //    }
 //}
 use App\Kernel;
