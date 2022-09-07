4b3e52990546f05d8fa0f56a5934d59d9d53521e
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-07 12:37:47 +0200

    Focus sur l'objet Response 1/2

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
-// En se basant sur l'exemple précédent enrichi de quelques messages, mettez en place un système
-// de pagination pour vos messages en ajoutant dans l'URL un paramètre page correspondant au numéro de la page.
-// Essayez par vous-même dans un premier temps. Si vous trouvez la tâche un peu trop ardue par cette
-// nouvelle notion de pagination, vous retrouverez dans la solution une aide vous expliquant pas à pas
-// comment la mettre en place.
+// Reprenons notre exemple précédent, c'est maintenant à vous d'ajouter quelques headers à
+// la réponse du contrôleur MessageController::item() :
+//
+//un header Cache-Control avec une durée d'une heure
+//
+//un header ETag contenant le hash md5 du contenu de notre réponse
+//
+
 //<?php
 //
 //// src/Controller/MessageController.php
@@ -12,20 +15,14 @@
 //
 //use App\Helper\MessageHelper;
 //use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Request;
 //use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 //use Symfony\Component\Routing\Annotation\Route;
 //
 //class MessageController extends AbstractController
 //{
 //    public const MESSAGES = ['Bonjour', 'Bonsoir', 'Au revoir', 'Salut', 'Coucou', 'Ciao', 'Adieu', 'Bienvenue', 'Bonne nuit', 'Bye'];
-//
-//    /**
-//     * @Route("/messages", name="message_list")
-//     */
-//    public function list(MessageHelper $messageHelper): Response
-//    {
-//        return $this->json($messageHelper->addUrlsToMessages(self::MESSAGES));
-//    }
 //
 //    /**
 //     * @Route("/messages/{id}", name="message_item")
@@ -35,10 +32,7 @@
 //        if (!isset(self::MESSAGES[$id])) {
 //            throw $this->createNotFoundException('Le message n\'existe pas');
 //        }
-//
-//        return $this->json(self::MESSAGES[$id]);
-//    }
-//}
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
