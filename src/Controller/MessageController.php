7526e4c958de72ee0a405cbf0523e7df7c309236
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-07 12:05:32 +0200

    Injecter un service dans un contrôleur 2/2

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,6 +3,7 @@
 // src/Controller/MessageController.php
 namespace App\Controller;
 
+use App\Helper\MessageHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,20 +12,19 @@ class MessageController extends AbstractController
 {
     public const MESSAGES = ['Bonjour', 'Bonsoir', 'Au revoir'];
 
-    /**
-     * @Route("/messages", name="message_list")
-     */
-    public function list(): Response
+    #[Route("/messages", name: "message_list")]
+    public function list(MessageHelper $messageHelper): Response
     {
-        $messagesWithURLs = [];
+        return $this->json($messageHelper->addUrlsToMessages(self::MESSAGES));
+    }
 
-        foreach (self::MESSAGES as $index => $message) {
-            $messagesWithURLs[] = [
-                'text' => $message,
-                'url' => $this->generateUrl('message_item', ['id' => $index]),
-            ];
+    #[Route("/messages/{id}", name: "message_item")]
+    public function item(int $id): Response
+    {
+        if (!isset(self::MESSAGES[$id])) {
+            throw $this->createNotFoundException('Le message n\'existe pas');
         }
 
-        return $this->json($messagesWithURLs);
+        return $this->json(self::MESSAGES[$id]);
     }
 }
